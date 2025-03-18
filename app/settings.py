816198685e9cd626391b6dfbf9637cd3b56ba5c5
Lostik46 <alekseyfedenko2006@gmail.com>
2025-03-18 23:05:53 +0300

    Update settings.py configuration

--- app/settings.py
+++ app/settings.py
@@ -87,7 +87,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static', 
+    os.path.join(BASE_DIR, 'static'),  
 ]
 
 # Media files (uploaded files)
